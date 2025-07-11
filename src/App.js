063a2fc2217177babf9daa5adfeae854f3b0017a
kromer52 <ilonakromer52@googlemail.com>
2025-07-11 09:05:41 +0200

    added container

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <header className="App-header">
         <p>
           Hello </p>
       </header>
+    </div>
     </div>
   );
 }
