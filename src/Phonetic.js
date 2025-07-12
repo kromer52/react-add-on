6574174c0b458d38cec8bf7ac9beb91aab73baa1
kromer52 <ilonakromer52@googlemail.com>
2025-07-12 14:54:26 +0200

    adding dummy code

---
+++ src/Phonetic.js
@@ -0,0 +1,6 @@
+// Photos.js
+import React from "react";
+
+export default function Photos(props) {
+  return <div className="Photos">[Photos will appear here later]</div>;
+}
---
+++ src/Synonyms.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  return <div className="Synonyms">[Synonyms will show here later]</div>;
+}
