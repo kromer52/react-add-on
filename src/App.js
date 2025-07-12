c391b817acac2bfe930a578a80a2aea971652381
kromer52 <ilonakromer52@googlemail.com>
2025-07-12 15:36:28 +0200

    troubleshooting non-functioning page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import Dictionary from "./Dictionary";
 export default function App() {
   console.log("App is rendering");
   return <h1>App is working</h1>;
 }
+
+console.log("Dictionary is:", Dictionary);
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,63 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Results from "./Results";
-import Photos from "./Photos";
-import "./Dictionary.css";
+import React, { useEffect } from "react";
 
 export default function Dictionary(props) {
-  const [keyword, setKeyword] = useState(props.defaultKeyword);
-  const [results, setResults] = useState(null);
-  const [photos, setPhotos] = useState(null);
+  console.log("► Dictionary mount – props.defaultKeyword:", props.defaultKeyword);
 
   useEffect(() => {
-    search();
-  }, []); // run once on mount
-
-  function handleDictionResponse(response) {
-    setResults(response.data[0]);
-  }
-
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
-  }
-
-  function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleDictionResponse);
-
-    let pexelsApiKey =
-      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
+    console.log("► Dictionary useEffect fired – you can stub search here");
+  }, []);
 
   return (
     <div className="Dictionary">
-      <section>
-        <h1>What word do you want to look up?</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            onChange={handleKeywordChange}
-            defaultValue={props.defaultKeyword}
-          />
-        </form>
-        <div className="hint">
-          suggested words: sunset, wine, yoga, plant...
-        </div>
-      </section>
-      <Results results={results} />
-      <Photos photos={photos} />
+      <h1>✅ Dictionary is rendering!</h1>
     </div>
   );
 }
